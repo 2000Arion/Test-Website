a6f46dcd82806ba3b8dd9c4123d2e44631ea71a2
2000Arion <91985694+2000Arion@users.noreply.github.com>
2022-08-10 15:14:21 +0200

    Update script.js

--- projects/urlparametertesting/script.js
+++ projects/urlparametertesting/script.js
@@ -18,7 +18,7 @@ function reset() {
     if (getParameter("format") == 2) {
         let html = '<a style="text-decoration:none;cursor:pointer;color:blue;"> Vielen Dank fürs Abbrechen als du darum gebeten wurdest! Als Belohnung darfs du nun auch auf den oberen Text klicken.</a>';
         document.getElementById("reset").innerHTML = html;
-        let html2 = '<a style="text-decoration:none;" href="?finish=true">Klick auf diesen Knopf! :)';
+        let html2 = '<a style="text-decoration:none;" href="?format=0&finish=true">Klick auf diesen Knopf! :)';
         document.getElementById("test").innerHTML = html2;
     } else if (getParameter("format") == null) {
         console.warn('This may not be executed here!')
