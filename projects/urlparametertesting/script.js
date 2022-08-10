b01e2fff7e07c56de55c5fd1edb07a9bdfd01280
2000Arion <91985694+2000Arion@users.noreply.github.com>
2022-08-10 14:19:02 +0200

    Update script.js

--- projects/urlparametertesting/script.js
+++ projects/urlparametertesting/script.js
@@ -14,22 +14,6 @@ function showPage() {
     document.getElementById("Div").style.display = "block";
 }
 
-
-if (getParameter("format") == "b") {
-    let html =
-        '<a id="test" style="text-decoration:none;" href="?format=i"><b>Hallo!</b> Klick doch nochmal :)</a>';
-    document.getElementById("test").innerHTML = html;
-}
-if (getParameter("format") == "i") {
-    let html =
-        '<a style="text-decoration:none;" href="?format=c"><b>Hallo!</b> <i>Bitte klick <b>nicht</b> nochmal...</i></a>';
-    document.getElementById("test").innerHTML = html;
-}
-if (getParameter("format") == "c") {
-    let html = "Och nee.. jetzt hör bitte wirklich auf...</a>";
-    document.getElementById("test").innerHTML = html;
-}
-
 function reset() {
     window.location.replace("/projects/urlparametertesting");
 }
