0ee342c55f52887b6d9c48b5715e9225350bb27a
2000Arion <91985694+2000Arion@users.noreply.github.com>
2022-08-10 15:09:24 +0200

    Added a "Win"

--- projects/urlparametertesting/script.js
+++ projects/urlparametertesting/script.js
@@ -15,5 +15,17 @@ function showPage() {
 }
 
 function reset() {
-    window.location.replace("/projects/urlparametertesting");
+    if (getParameter("format") == 2) {
+        let html = '<a style="text-decoration:none;cursor:pointer;color:blue;"> Vielen Dank fürs Abbrechen als du darum gebeten wurdest! Als Belohnung darfs du nun auch auf den oberen Text klicken.</a>';
+        document.getElementById("reset").innerHTML = html;
+        let html2 = '<a style="text-decoration:none;" href="?finish=true">Klick auf diesen Knopf! :)';
+        document.getElementById("test").innerHTML = html2;
+    } else if (getParameter("format") == null) {
+        console.warn('This may not be executed here!')
+    } else {
+        window.location.replace("/upt");
+    }
+}
+function finishreset() {
+    window.location.replace("/upt")
 }
